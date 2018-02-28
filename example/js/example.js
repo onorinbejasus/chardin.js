caf5375a0be27498f09e8643d2624a3ae65c5957
onorinbejasus <tluciani21@gmail.com>
2018-02-28 12:27:22 -0600

    -- Moved a class from the demo.css to the library file -- Added a z-index

--- example/js/example.js
+++ example/js/example.js
@@ -1,15 +1,15 @@
 (function(){
 
   function init() {
-    var svg = d3.select("svg"),
+    let svg = d3.select("svg"),
       margin = {top: 20, right: 20, bottom: 30, left: 40},
       width = +svg.attr("width") - margin.left - margin.right,
       height = +svg.attr("height") - margin.top - margin.bottom;
 
-    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
       y = d3.scaleLinear().rangeRound([height, 0]);
 
-    var g = svg.append("g")
+    let g = svg.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     d3.tsv("./data.tsv", function(d) {
